b25799196482bfa731b0f6ba2b5b4926ed50ab45
Balazs Varga <bvarga@cloudera.com>
2021-03-31 18:56:15 +0200

    Add visualization module, hello world example

--- src/main/java/main/TadpoleExploration.java
+++ src/main/java/main/TadpoleExploration.java
@@ -1,5 +1,23 @@
+package main;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 public class TadpoleExploration {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.setProperty("org.graphstream.ui", "swing");
+
+        Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+
+        graph.display();
     }
 }
