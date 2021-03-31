55456d6f3ae66fffd1315073a89cbf8aa05dcb48
Balazs Varga <bvarga@cloudera.com>
2021-03-31 15:31:17 +0200

    Initial commit

---
+++ src/main/java/main/TadpoleExploration.java
@@ -0,0 +1,5 @@
+public class TadpoleExploration {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/model/GreedyAlgorithm.java
@@ -0,0 +1,33 @@
+package model;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.GraphFactory;
+
+public class GreedyAlgorithm {
+
+    public GreedyAlgorithm(int cycleLength, int tailLength, int startNode) {
+        throw new UnsupportedOperationException("Todo implement");
+        // start node like this:
+        /*          /5-6\
+           0-1-2-3-4    7
+                    \9-8/
+         */
+    }
+
+    public void nextStep() {
+        throw new UnsupportedOperationException("Todo implement");
+        // perform a step in the algorithm
+    }
+
+    public Graph getCurrentGraph() {
+        throw new UnsupportedOperationException("Todo implement");
+    }
+
+    public int getCurrentCost() {
+        throw new UnsupportedOperationException("Todo implement");
+    }
+
+    public boolean isFinished() {
+        throw new UnsupportedOperationException("Todo implement");
+    }
+}
